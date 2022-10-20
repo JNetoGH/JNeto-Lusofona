86c1490767e8e7e9a1301ea67653fe97593cc6d4
JNeto <joaomendes15@gmail.com>
2022-10-20 23:14:18 +0100

    trab computcao refinement 1

--- Introducao a Computacao/6) Mapa RPG/main.py
+++ Introducao a Computacao/6) Mapa RPG/main.py
@@ -1,75 +1,57 @@
-from enum import Enum
-
-positions = [["a", "b", "c"],
-             ["d", "e", "f"],
-             ["g", "h", "i"]]
+map = [["a", "b", "c"],
+       ["d", "e", "f"],
+       ["g", "h", "i"]]
 
 
 def print_map():
     print("MAPA", end="")
-    for i in range(0, len(positions)):
+    for i in range(0, len(map)):
         print("\n|", end="")
-        for j in range(0, len(positions[0])):
-            print(f" {positions[i][j]}", end=" |")
+        for j in range(0, len(map[0])):
+            print(f" {map[i][j]}", end=" |")
     print()
 
 
-class AxisOP(Enum):
-    Horizontal = 0,
-    Vertical = 1
+class Axis:
+    HORIZONTAL: int = 1
+    VERTICAL: int = 0
 
 class Player:
 
     def __init__(self, initial_pos: list[int]):
-        self.currentPosAsAxis: list[int] = initial_pos
+        self.pos: list[int] = initial_pos
         print("initial ", end="")
-        self.print_current_pos()
+        self.print_pos_status()
 
-    def move(self, move_code):
+    def move(self, code):
         print_map()
-        print("You tried to move: " + move_code)
+        print("You tried to move: " + code)
 
-        if move_code == "EAST" and self.currentPosAsIndex(AxisOP.Horizontal) + 1 < len(positions[0]):
-            self.currentPosAsAxis[1] = self.currentPosAsAxis[1] + 1
-            self.print_current_pos()
+        if code == "EAST" and self.pos[Axis.HORIZONTAL] + 1 < len(map[0]):
+            self.pos[Axis.HORIZONTAL] += 1
+            self.print_pos_status()
             return
-        elif move_code == "WEST" and self.currentPosAsIndex(AxisOP.Horizontal) - 1 >= 0:
-            self.currentPosAsAxis[1] = self.currentPosAsAxis[1] - 1
-            self.print_current_pos()
+        elif code == "WEST" and self.pos[1] - 1 >= 0:
+            self.pos[Axis.HORIZONTAL] -= 1
+            self.print_pos_status()
             return
-        elif move_code == "SOUTH" and self.currentPosAsIndex(AxisOP.Vertical) + 1 < len(positions):
-            self.currentPosAsAxis[0] = self.currentPosAsAxis[0] + 1
-            self.print_current_pos()
+        elif code == "SOUTH" and self.pos[Axis.VERTICAL] + 1 < len(map):
+            self.pos[Axis.VERTICAL] += 1
+            self.print_pos_status()
             return
-        elif move_code == "NORTH" and self.currentPosAsIndex(AxisOP.Vertical) -1 >= 0:
-            self.currentPosAsAxis[0] = self.currentPosAsAxis[0] - 1
-            self.print_current_pos()
+        elif code == "NORTH" and self.pos[Axis.VERTICAL] - 1 >= 0:
+            self.pos[Axis.VERTICAL] -= 1
+            self.print_pos_status()
             return
-
         print("NOT VALID")
-        self.print_current_pos()
 
-    def currentPosAsIndex(self, axis: AxisOP):
-        for x in range(0, len(positions)):
-            for y in range(0, len(positions[0])):
-                if positions[x][y] == positions[self.currentPosAsAxis[0]][self.currentPosAsAxis[1]]:
-                    if axis == AxisOP.Horizontal:
-                        return y
-                    elif axis == AxisOP.Vertical:
-                        return x
-        return -1
-
-    def print_current_pos(self):
-        print("pos: " + positions[self.currentPosAsAxis[0]][self.currentPosAsAxis[1]])
-        print(f"current index: [{self.currentPosAsIndex(AxisOP.Vertical)}][{self.currentPosAsIndex(AxisOP.Horizontal)}]")
-        print()
+    def print_pos_status(self):
+        print("pos: " + map[self.pos[Axis.VERTICAL]][self.pos[Axis.HORIZONTAL]])
+        print(f"current index: [{self.pos[Axis.VERTICAL]}][{self.pos[Axis.HORIZONTAL]}]\n")
 
 
 print_map()
 joao = Player([0, 1])  # x, y
-
 while True:
     value = input("choose a direction: ").upper()
     joao.move(value)
-
-
